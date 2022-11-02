ad8757743a24503cbd2e3de64d25afceab0bea0f
actuallyfro <actuallyfro@github.com>
2022-11-02 11:27:04 -0400

    {Day 2 README} Adding before fixing

---
+++ 02_Day-02_Notes/GenerateREADME.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+outFile="README.adoc"
+echo "= Day 01 Notes" > $outFile
+
+for file in *.adoc; do
+  if [ "$file" != "$outFile" ]; then
+    title=$(grep -m 1 "^= " $file)
+    title=${title:2}
+
+    # get two digit number from file name
+    num=$(echo $file | grep -oE '[0-9]{2}')
+
+    echo "$num. Notes Title: \`"$title"\`">> $outFile
+    echo "link:$file[$title]" >> $outFile
+
+    # echo "[DEBUG] Looking for: $slidesPDF"
+    if [ -f "$num"_*.pdf ]; then
+      #save file name in var
+      slidesPDF=$(ls $num*.pdf)
+
+      # echo "[DEBUG] PDF/slides ($slidesPDF) MATCH FOUND for $title"
+      echo "link:$slidesPDF[PDF]" >> $outFile
+    fi
+ 
+    echo "" >> $outFile
+  fi
+  
+done
